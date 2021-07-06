fc55a5e6cc8aaeb1f09811d84734eb1a957ff639
Vishnu M P <57670201+vishnump1592@users.noreply.github.com>
2021-07-06 18:07:41 +0530

    UIButton Extension

---
+++ UIButton+Extension .swift
@@ -0,0 +1,113 @@
+//
+//  UIButton+Extension .swift
+//  iOS Coding
+//
+//  Created by Vishnu M P on 06/07/21.
+//
+
+import UIKit
+
+public extension UIButton {
+    
+    private func animateScale(to scale: CGFloat, duration: TimeInterval) {
+        UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: [], animations: {
+            self.transform = .init(scaleX: scale, y: scale)
+        }, completion: nil)
+    }
+    
+    func alignTextBelow(spacing: CGFloat = 6.0) {
+        if let image = self.imageView?.image {
+            let imageSize: CGSize = image.size
+            self.titleEdgeInsets = UIEdgeInsets(top: spacing, left: -imageSize.width, bottom: -(imageSize.height), right: 0.0)
+            let labelString = NSString(string: self.titleLabel!.text!)
+            let titleSize = labelString.size(withAttributes: [NSAttributedString.Key.font: self.titleLabel!.font!])
+            self.imageEdgeInsets = UIEdgeInsets(top: -(titleSize.height + spacing), left: 0.0, bottom: 0.0, right: -titleSize.width)
+        }
+    }
+    
+    func addBottomBorder(){
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 1)
+        bottomLine.backgroundColor = UIColor.lightGray.cgColor
+        layer.addSublayer(bottomLine)
+    }
+    
+    func setCircleImage(){
+        self.layer.cornerRadius = self.frame.size.height/2
+        self.layer.masksToBounds = true
+        self.layer.borderWidth = 1.0
+        self.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    func setButtonBorderWithColor(borderWidth : CGFloat, borderColor : UIColor){
+        self.layer.masksToBounds = true
+        self.layer.borderWidth = CGFloat(borderWidth)
+        self.layer.borderColor = borderColor.cgColor
+    }
+    
+    func roundCornersButton(corners:UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+    }
+    
+    func hasImage(named imageName: String, for state: UIControl.State) -> Bool {
+        guard let buttonImage = image(for: state), let namedImage = UIImage(named: imageName) else { return false }
+        
+        return buttonImage.pngData() == namedImage.pngData()
+    }
+    
+    func pulsate() {
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        pulse.duration = 0.6
+        pulse.fromValue = 0.8
+        pulse.toValue = 1.0
+        pulse.autoreverses = false
+        pulse.repeatCount = 0
+        pulse.initialVelocity = 0.5
+        pulse.damping = 1.0
+        layer.add(pulse, forKey: "pulse")
+    }
+    
+    func flash() {
+        let flash = CABasicAnimation(keyPath: "opacity")
+        flash.duration = 0.2
+        flash.fromValue = 1
+        flash.toValue = 0.1
+        flash.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        flash.autoreverses = true
+        flash.repeatCount = 1
+        layer.add(flash, forKey: nil)
+    }
+    
+    
+    func shake() {
+        let shake = CABasicAnimation(keyPath: "position")
+        shake.duration = 0.1
+        shake.repeatCount = 2
+        shake.autoreverses = true
+        let fromPoint = CGPoint(x: center.x - 5, y: center.y)
+        let fromValue = NSValue(cgPoint: fromPoint)
+        let toPoint = CGPoint(x: center.x + 5, y: center.y)
+        let toValue = NSValue(cgPoint: toPoint)
+        shake.fromValue = fromValue
+        shake.toValue = toValue
+        layer.add(shake, forKey: "position")
+    }
+    
+    func addRightIcon(_ image: UIImage) {
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(imageView)
+        let length = CGFloat(15)
+        titleEdgeInsets.right += length
+        
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: self.titleLabel!.trailingAnchor, constant: 10),
+            imageView.centerYAnchor.constraint(equalTo: self.titleLabel!.centerYAnchor, constant: 0),
+            imageView.widthAnchor.constraint(equalToConstant: length),
+            imageView.heightAnchor.constraint(equalToConstant: length)
+        ])
+    }
+}
